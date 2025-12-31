895f457ba1ad2b51bcb1fec41103bbb05041e1f6
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2025-12-31 10:16:41 +0100

    la structure de projet OOP

---
+++ Classes/Person.php
@@ -0,0 +1,41 @@
+<?php
+abstract class Person
+{
+    private int $id;
+    private string $name, $nationnalite;
+
+    public function __construct($id, $name, $nationnalite)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->nationnalite = $nationnalite;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function setName($name): string
+    {
+        if ($name == null) {
+            throw new Exception("Le nom est obligatoire", 1);
+        }
+        return $this->name = $name;
+    }
+    public function getNat(): string {
+        return $this->nationnalite ;
+    }
+    public function setNat($nationnalite):string{
+        if ($nationnalite == null ) {
+            throw new Exception("La nationnalite est obligatoire");
+        }
+        return $this->nationnalite;
+    }
+    abstract public function getAnnualCost() : float;
+    
+}
