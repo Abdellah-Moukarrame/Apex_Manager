cbc0db25b0ce6a9a8dbd8b44477101e5ca13b30e
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-09 15:55:03 +0100

    update the roadster class

--- Classes/Roaster.php
+++ Classes/Roaster.php
@@ -1,5 +1,5 @@
 <?php
-require "../../DB/db_connect.php";
+require_once "../../DB/db_connect.php";
 class Roaster
 {
     // private string  $name, $role, $pseudo;
@@ -14,19 +14,21 @@ class Roaster
 
     //Pagination Process
 
-    public function count(){
-        $sql_count =$this->db->prepare( "select count(*) as total_players from joueur");
+    public function count()
+    {
+        $sql_count = $this->db->prepare("select count(*) as total_players from joueur");
         $sql_count->execute();
         $fetch_total = $sql_count->fetchColumn();
-        return $fetch_total ;
+        return $fetch_total;
     }
-    public function displayBypage($page){
+    public function displayBypage($page)
+    {
         //hna 7dedt l offsit
-       $page = ($page - 1) * 4;
-       //hna jebt players
-       $sql_display = $this->db->prepare("select joueur.* , contrat.salaire , contrat.date_fin from joueur left join contrat on contrat.id_joueur = joueur.id_J limit 4 offset $page");
-       $sql_display->execute();
-       $fetch = $sql_display->fetchAll();
-       return  $fetch ;
+        $page = ($page - 1) * 4;
+        //hna jebt players
+        $sql_display = $this->db->prepare("select equipe.nom_E , joueur.* , contrat.salaire , contrat.date_fin from joueur left join contrat on contrat.id_joueur = joueur.id_J join equipe on equipe.id_E = contrat.id_Equipe limit 4 offset $page");
+        $sql_display->execute();
+        $fetch = $sql_display->fetchAll();
+        return  $fetch;
     }
 }
